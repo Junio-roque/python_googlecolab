8dab02110491cca548ba1d0e429295f682196a3e
JC <cezar_junio@yahoo.com.br>
2023-07-27 17:24:26 -0300

    teste

--- primeiroProz.py
+++ primeiroProz.py
@@ -3,12 +3,12 @@ veiculoQuantidadeDeRoda = int(input("Quantas rodas o seu veiculo tem: "))
 peso = int(input("Qual o peso: "))
 
 if(veiculoQuantidadeDeRoda >= 4) and (peso > 6000):
-  print("Veiculo de quatro roda e com peso mais de 6000kg.")
+  print("E")
 elif(veiculoQuantidadeDeRoda >= 4) and (quantidadeDePessoas > 8):
-  print("Veiculo de quatro ou mais rodas que acomoda mais de oito pessoas.")
+  print("D")
 elif(veiculoQuantidadeDeRoda >= 4) and (peso >= 3500 or peso <= 6000):
-  print("Veiculo com quatro rodas ou mais com peso entre 3500kg a 6000kg")
+  print("C")
 elif(veiculoQuantidadeDeRoda == 4) and (peso >= 3500 and quantidadeDePessoas <= 8):
-  print("Veiculo com quatro rodas que pode acomodar ate oito pessoas de 3500kg")
+  print("B")
 else :
- print("Veiulo com duas ou três rodas.")
+ print("A")
